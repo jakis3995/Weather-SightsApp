4d74a412a58eade67221a6bd919627e48c7f6c85
Maksim Klevtsov <max39.95@mail.ru>
2020-02-18 11:01:48 +0200

    Initial Commit

---
+++ Weather&SightsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather&SightsApp
+//
+//  Created by Максим Клевцов  on 18/02/2020.
+//  Copyright © 2020 Максим Клевцов . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
